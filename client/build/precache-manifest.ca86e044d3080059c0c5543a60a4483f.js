0b94eaf625ece3d35fdf25256de235e948f65891
Manansh Shukla <mananshshukla@Mananshs-MacBook-Air.local>
2020-11-19 20:57:54 -0700

    Articulate Deployed

---
+++ client/build/precache-manifest.ca86e044d3080059c0c5543a60a4483f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c31c05ef2d868592d790f22b825362b",
+    "url": "/Articulate/index.html"
+  },
+  {
+    "revision": "545833b219dce5300cf5",
+    "url": "/Articulate/static/css/2.aab2990a.chunk.css"
+  },
+  {
+    "revision": "a6d455148c2be3f4007c",
+    "url": "/Articulate/static/css/main.b585f5bd.chunk.css"
+  },
+  {
+    "revision": "545833b219dce5300cf5",
+    "url": "/Articulate/static/js/2.8bba56ac.chunk.js"
+  },
+  {
+    "revision": "b636b9e2f0dd66c1d571afdd9c22a537",
+    "url": "/Articulate/static/js/2.8bba56ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d455148c2be3f4007c",
+    "url": "/Articulate/static/js/main.98a03cb0.chunk.js"
+  },
+  {
+    "revision": "fdcba3c7bbb46d240a44",
+    "url": "/Articulate/static/js/runtime-main.480c915f.js"
+  },
+  {
+    "revision": "5415f395c1567a5c19efc1dc2892927a",
+    "url": "/Articulate/static/media/assistant-light.5415f395.ttf"
+  },
+  {
+    "revision": "e2b46dd69f54e57767ceef1d5fc8e688",
+    "url": "/Articulate/static/media/assistant-regular.e2b46dd6.ttf"
+  },
+  {
+    "revision": "ca876553b68e11c40b55600fe558b250",
+    "url": "/Articulate/static/media/favicon.ca876553.ico"
+  },
+  {
+    "revision": "1ecabc2994c9653271f2241747089145",
+    "url": "/Articulate/static/media/glacial-indifference-bold.1ecabc29.ttf"
+  },
+  {
+    "revision": "2abec0199dc03014a92e3350b3674bae",
+    "url": "/Articulate/static/media/glacial-indifference-regular.2abec019.ttf"
+  },
+  {
+    "revision": "44120470252ec68c7b09fd344b5c1e75",
+    "url": "/Articulate/static/media/header.44120470.png"
+  },
+  {
+    "revision": "4c155ee90b2eac6a89566be30bcb65c3",
+    "url": "/Articulate/static/media/megaphone.4c155ee9.png"
+  }
+]);
